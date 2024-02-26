f93b8daed0a83990753570c9956fb9c0b79dde0b
SimonLee0316 <bb32739451@gmail.com>
2024-02-26 23:10:04 +0800

    Complete q_free() implement Creat a new pointer type is a pointer to list_head, Assign head->next to this new pointer name as current,then start while loop end with The situation which  current equal to head. At the end,free the head.

--- queue.c
+++ queue.c
@@ -24,7 +24,20 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    struct list_head *current = head->next;
+
+    while (head != current) {
+        q_release_element(container_of(current, element_t, list));
+        current = current->next;
+    }
+
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
