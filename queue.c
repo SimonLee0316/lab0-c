b9f995092c2719d147f0e740474994d052dd458d
SimonLee0316 <bb32739451@gmail.com>
2024-02-28 19:14:44 +0800

    Complete q_remove_head() implement
    Remove an element from head of queue.
    Copy the (first element)->value to the sp which size is bufsize.

--- queue.c
+++ queue.c
@@ -83,7 +83,16 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+    element_t *felement = list_first_entry(head, element_t, list);
+    list_del_init(head->next);
+
+    if (!sp)
+        return NULL;
+    strncpy(sp, felement->value, bufsize);
+
+    return felement;
 }
 
 /* Remove an element from tail of queue */
