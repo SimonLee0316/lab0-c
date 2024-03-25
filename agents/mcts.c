5e7137128eb7c339a61d6091982ca22d580e1733
simon <bb32739451@gmail.com>
2024-03-26 04:56:29 +0800

    Converting logarithm with base2 to natural
    Using Taylor series expansion to find the natural logarithm.

--- agents/mcts.c
+++ agents/mcts.c
@@ -38,28 +38,23 @@ static void free_node(struct node *node)
     free(node);
 }
 
-const int tab64[64] = {63, 0,  58, 1,  59, 47, 53, 2,  60, 39, 48, 27, 54,
-                       33, 42, 3,  61, 51, 37, 40, 49, 18, 28, 20, 55, 30,
-                       34, 11, 43, 14, 22, 4,  62, 57, 46, 52, 38, 26, 32,
-                       41, 50, 36, 17, 19, 29, 10, 13, 21, 56, 45, 25, 31,
-                       35, 16, 9,  12, 44, 24, 15, 8,  23, 7,  6,  5};
-
-__uint64_t log2_64(__uint64_t value)
-{
-    value |= value >> 1;
-    value |= value >> 2;
-    value |= value >> 4;
-    value |= value >> 8;
-    value |= value >> 16;
-    value |= value >> 32;
-    return tab64[((__uint64_t) ((value - (value >> 1)) * 0x07EDD5E59A4E28C2)) >>
-                 58];
-}
-
 static __uint64_t do_log(__uint64_t x)
 {
-    // printf("do_log :%lu\n",x);
-    return log2_64(x);
+    __uint64_t x_plus_1 = x + (1U << SHIFT_AMOUNT);
+    __uint64_t x_minus_1 = x - (1U << SHIFT_AMOUNT);
+    __uint64_t tmp1 = (x_minus_1 << SHIFT_AMOUNT) / x_plus_1;
+    __uint64_t tmp2 = (tmp1 * tmp1) >> SHIFT_AMOUNT;
+    __uint64_t result = 1U << SHIFT_AMOUNT;
+    for (__uint64_t i = 1; i < 10; i++) {
+        __uint64_t term =
+            (tmp2 << SHIFT_AMOUNT) / ((2 * i + 1) << SHIFT_AMOUNT);
+        result += term;
+        tmp2 = (tmp2 * tmp2) >> SHIFT_AMOUNT;
+    }
+
+    result <<= 1;
+    result = (result * tmp1) >> SHIFT_AMOUNT;
+    return result;
 }
 
 static __uint64_t do_sqrt(__uint64_t x)
